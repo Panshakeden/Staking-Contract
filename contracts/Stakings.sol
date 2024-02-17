8a9d24833761af79ae83b520a2592b31a0e21576
panshak <panshakeden@gmail.com>
2024-02-17 18:16:04 +0100

    initial commit

--- contracts/Stakings.sol
+++ contracts/Stakings.sol
@@ -37,9 +37,11 @@ contract Stakings {
         }
 
         require(token.balanceOf(msg.sender) >=  amount,"no enough token");
+        
         require(token.transferFrom(msg.sender, address(this), amount),"failed to transfer");
 
         stakedBalance[msg.sender]= stakedBalance[msg.sender] + amount;
+
         lastStakedTime[msg.sender]= block.timestamp;
     }
 
@@ -56,7 +58,7 @@ contract Stakings {
         }
         // require(block.timestamp >= lastStakedTime[msg.sender]);
         if ((lastStakedTime[msg.sender])>block.timestamp){
-            
+
             revert TIME_CONFLICT_ERROR();
         }
           
